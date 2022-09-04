c92d29aac9838a99cd5553144332cb9b01a99e78
TAEGYU JANG <xorb2298@gmail.com>
2022-09-05 02:06:41 +0900

    Add Response method for view model

--- app/src/main/java/com/example/wanted_preonboarding_android/ui/NewsViewModel.kt
+++ app/src/main/java/com/example/wanted_preonboarding_android/ui/NewsViewModel.kt
@@ -7,18 +7,19 @@ import com.example.wanted_preonboarding_android.models.NewsResponse
 import com.example.wanted_preonboarding_android.repository.NewsRepository
 import com.example.wanted_preonboarding_android.utils.Resource
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class NewsViewModel(
     val newsRepository: NewsRepository
 ) : ViewModel() {
 
     val topHeadlinesNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    val topHeadlinesNewsPage = 1
-    val topHeadlinesNewsResponse: NewsResponse? = null
+    var topHeadlinesNewsPage = 1
+    var topHeadlinesNewsResponse: NewsResponse? = null
 
     val categoryNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
-    val categoryNewsPage = 1
-    val categoryNewsResponse: NewsResponse? = null
+    var categoryNewsPage = 1
+    var categoryNewsResponse: NewsResponse? = null
 
     init {
         getTopHeadlineNews("us")
@@ -26,6 +27,24 @@ class NewsViewModel(
 
     fun getTopHeadlineNews(countryCode: String) = viewModelScope.launch {
         topHeadlinesNews.postValue(Resource.Loading())
+        val response = newsRepository.getTopHeadlineNews(countryCode, topHeadlinesNewsPage)
+        topHeadlinesNews.postValue(handleTopHeadlinesNewsResponse(response))
+    }
 
+    private fun handleTopHeadlinesNewsResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
+        if(response.isSuccessful) {
+            response.body()?.let { resultResponse ->
+                topHeadlinesNewsPage++
+                if (topHeadlinesNewsResponse == null) {
+                    topHeadlinesNewsResponse = resultResponse
+                } else {
+                    val olArticles = topHeadlinesNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    olArticles?.addAll(newArticles)
+                }
+                return Resource.Success(topHeadlinesNewsResponse ?: resultResponse)
+            }
+        }
+        return Resource.Error(response.message())
     }
 }
