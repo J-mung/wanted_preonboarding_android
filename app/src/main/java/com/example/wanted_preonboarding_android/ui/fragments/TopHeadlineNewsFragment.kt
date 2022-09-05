99d4b0f205bfe97021c33c0934ad0b3db8a493c5
TAEGYU JANG <xorb2298@gmail.com>
2022-09-05 20:11:42 +0900

    Add fragment layouts & topHeadlineNews fragment

---
+++ app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/TopHeadlineNewsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.wanted_preonboarding_android.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.wanted_preonboarding_android.R
+import com.example.wanted_preonboarding_android.ui.NewsViewModel
+
+class TopHeadlineNewsFragment : Fragment(R.layout.fragment_topheadline_news) {
+
+    lateinit var viewModel: NewsViewModel
+
+}
