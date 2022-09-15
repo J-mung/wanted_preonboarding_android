272bd361f0f16fd7961f73d88adaeb03d26ae36a
TAEGYU JANG <xorb2298@gmail.com>
2022-09-15 16:58:19 +0900

    Add layout & resource of CategoryNewsFragment

---
+++ app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/CategoryListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.wanted_preonboarding_android.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.example.wanted_preonboarding_android.R
+
+class CategoryListFragment : Fragment(R.layout.fragment_category_list){
+
+}
