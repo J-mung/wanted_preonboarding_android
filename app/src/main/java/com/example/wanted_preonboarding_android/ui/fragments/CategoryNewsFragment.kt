92ff0a371bc43d9db91520d6b0810588132bf375
TAEGYU JANG <xorb2298@gmail.com>
2022-09-17 20:14:45 +0900

    Edit CategoryNews func

--- app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/CategoryNewsFragment.kt
+++ app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/CategoryNewsFragment.kt
@@ -1,22 +1,136 @@
 package com.example.wanted_preonboarding_android.ui.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AbsListView
+import android.widget.ProgressBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.wanted_preonboarding_android.R
 import com.example.wanted_preonboarding_android.adapter.NewsAdapter
 import com.example.wanted_preonboarding_android.ui.NewsViewModel
+import com.example.wanted_preonboarding_android.utils.Constants.Companion.QUERY_PAGE_SIZE
+import com.example.wanted_preonboarding_android.utils.Resource
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
-class CategoryNewsFragment : Fragment(R.layout.fragment_category_news){
+class CategoryNewsFragment : Fragment(R.layout.fragment_category_news) {
 
-    private val viewModel : NewsViewModel by activityViewModels()
-    lateinit var newsAdapter : NewsAdapter
+    private val viewModel: NewsViewModel by activityViewModels()
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var rvCategoryNews: RecyclerView
+    lateinit var paginationProgressBar: ProgressBar
+    lateinit var category: String
 
     val TAG = "CategoryNewsFragment"
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        category = arguments?.getString("category_txt").toString()
+        rvCategoryNews = view.findViewById(R.id.rvCategoryNews)
+        setUpRecyclerView()
 
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+
+            findNavController().navigate(
+                R.id.action_categoryNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+
+        var job: Job? = null
+        if (category.toString().isNotEmpty()) {
+            job?.cancel()
+            job = MainScope().launch {
+                viewModel.getCategoryNews("us", category)
+            }
+        }
+
+        paginationProgressBar = view.findViewById(R.id.paginationProgressBar)
+        viewModel.categoryNews.observe(viewLifecycleOwner, Observer { response ->
+            when (response) {
+                is Resource.Success -> {
+                    hideProgressBar()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / QUERY_PAGE_SIZE + 2
+                        isLastPage = viewModel.categoryNewsPage == totalPages
+                        if(isLastPage) {
+                            rvCategoryNews.setPadding(0, 0, 0, 0)
+                        }
+                    }
+                }
+                is Resource.Error -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Log.e(TAG, "An error accrued: $message")
+                    }
+                }
+                is Resource.Loading -> {
+                    showProgressBar()
+                }
+            }
+        })
+    }
+
+    private fun showProgressBar() {
+        paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    private fun hideProgressBar() {
+        paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    var scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val sholdPaginate =
+                isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if(sholdPaginate) {
+                viewModel.getCategoryNews("us", category)
+                isScrolling = false
+            }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        newsAdapter = NewsAdapter()
+        rvCategoryNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@CategoryNewsFragment.scrollListener)
+        }
     }
 }
