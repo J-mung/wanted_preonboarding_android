c021831c13c8cd5d99411cd27bf0d9bbcd7fe61f
TAEGYU JANG <xorb2298@gmail.com>
2022-09-23 18:22:24 +0900

    Add SavedNews func()

--- app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/wanted_preonboarding_android/ui/fragments/ArticleFragment.kt
@@ -9,15 +9,19 @@ import androidx.navigation.fragment.navArgs
 import com.example.wanted_preonboarding_android.R
 import com.example.wanted_preonboarding_android.ui.MainActivity
 import com.example.wanted_preonboarding_android.ui.NewsViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
     lateinit var viewModel: NewsViewModel
     lateinit var webView: WebView
+    lateinit var btnFab: FloatingActionButton
     val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+        btnFab = view.findViewById(R.id.btnFab)
 
         val article = args.article
         webView = view.findViewById<WebView>(R.id.webView)
@@ -26,5 +30,9 @@ class ArticleFragment : Fragment(R.layout.fragment_article) {
             loadUrl(article.url)
         }
 
+        btnFab.setOnClickListener{
+            viewModel.saveArticle(article)
+            Snackbar.make(view, "Article Saved Successfully", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
